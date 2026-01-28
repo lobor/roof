764ff5064a57e5e30378dcff78bdea644595307c
lobor <lionel.bertrand@paymium.com>
2026-01-28 10:12:27 +0100

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,19 +7,19 @@ export default function Document() {
       <body className="antialiased sticky-header">
         <div id="preloader"></div>
         <Main />
-        <script src="/js/jquery-3.3.1.min.js"></script>
-        <script src="/js/plugins.js"></script>
-        <script src="/js/popper.min.js"></script>
-        <script src="/js/bootstrap.min.js"></script>
-        <script src="/js/jquery.meanmenu.min.js"></script>
-        <script src="/vendor/slick/slick.min.js"></script>
-        <script src="/js/imagesloaded.pkgd.min.js"></script>
+        <script src="/roof/js/jquery-3.3.1.min.js"></script>
+        <script src="/roof/js/plugins.js"></script>
+        <script src="/roof/js/popper.min.js"></script>
+        <script src="/roof/js/bootstrap.min.js"></script>
+        <script src="/roof/js/jquery.meanmenu.min.js"></script>
+        <script src="/roof/vendor/slick/slick.min.js"></script>
+        <script src="/roof/js/imagesloaded.pkgd.min.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtmXSwv4YmAKtcZyyad9W7D4AC08z0Rb4"></script>
-        <script src="/js/select2.min.js"></script>
-        <script src="/js/jquery.magnific-popup.min.js"></script>
-        <script src="/js/jquery.counterup.min.js"></script>
-        <script src="/js/waypoints.min.js"></script>
-        <script src="/js/validator.min.js"></script>
+        <script src="/roof/js/select2.min.js"></script>
+        <script src="/roof/js/jquery.magnific-popup.min.js"></script>
+        <script src="/roof/js/jquery.counterup.min.js"></script>
+        <script src="/roof/js/waypoints.min.js"></script>
+        <script src="/roof/js/validator.min.js"></script>
         {/*<script src="/js/main.js"></script>*/}
         <NextScript />
       </body>
